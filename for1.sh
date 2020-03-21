04c0b8faa465a9f764d2d12b626d09b9ddebf69b
hosam1696 <hosamelnabawy1996@gmail.com>
2020-03-21 04:36:36 +0200

    Update Readme & Add helper notes

--- for1.sh
+++ for1.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+###
+# This script is basicaly used to capture the input from the user 
+# when he enter the inquiry and enter his friends names
+# then the script will display the input in nice format.
+###
+
+
 slugs=(first second third forth fifth)
 friends_names=()
 echo
@@ -7,8 +15,8 @@ echo "----------------------------------------------"
 for i in 0 1 2 3 4
 	do 
 		read -p "〇 ${slugs[$i]} Name is? " name
-		if [ -z "$names[$i]" ]; then
-                echo "You have to enter a name"
+		if [ -z "$name" ]; then
+                echo "You have to enter a valid name"
 		        exit	
         fi
         # echo
