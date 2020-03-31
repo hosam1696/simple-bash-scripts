ae61f20a9bbf5ee6d720fda8097e17042eb338fe
hosam1696 <hosamelnabawy1996@gmail.com>
2020-03-31 22:14:49 +0200

    Edit if bash script file and write docs in the file

--- if2.sh
+++ if2.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+###
+# This script is used for checking if the file exists or not
+# by prompting the user to enter the file path then excute
+# `stat` command to get more details about the file 
+###
+
 
 echo -e "Enter File Name to Test: \c"
 read file_name
 
-stat $file_name
 
-
-if (( -f $file_nam))
-    then
-        echo $file_name is Found
-    else
-        echo "File NOT Found"
+if [[ -f "$file_name" ]];then
+	stat $file_name    	
+else
+    	echo "File NOT Found"
 fi
